c80e31598b3aacd6a37500fac18738dc80af8822
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-08-09 16:08:54 +0200

    Simple Forms, ViewChild(ren)

--- book-rating/src/app/dashboard/dashboard.component.ts
+++ book-rating/src/app/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, AfterViewInit, ViewChild, ViewChildren, QueryList } from '@angular/core';
 
+import { BookComponent } from '../book/book.component';
 import { Book } from '../shared/book';
 
 @Component({
@@ -7,7 +8,12 @@ import { Book } from '../shared/book';
   templateUrl: './dashboard.component.html',
   styleUrls: ['./dashboard.component.css']
 })
-export class DashboardComponent implements OnInit {
+export class DashboardComponent implements OnInit, AfterViewInit {
+
+  @ViewChild('isbnEl') isbn: ElementRef;
+  @ViewChild('titleEl') title: ElementRef;
+  @ViewChild('descriptionEl') description: ElementRef;
+  @ViewChildren(BookComponent) bookComps: QueryList<BookComponent>;
 
   imgUrl = 'https://ng-buch.de/avatar/12';
 
@@ -18,12 +24,22 @@ export class DashboardComponent implements OnInit {
       new Book('000', 'Angular', 'Zurück in die Zukunft', 5),
       new Book('111', 'AngularJS 1.x', 'Oldie but goldie', 3)
     ];
-
-    this.foo('Hello world');
   }
 
-  foo(text: string) {
-    console.log(text);
+
+  ngAfterViewInit() {
+    console.log(this.bookComps);
   }
 
+  add() {
+    const newBook = new Book(
+      this.isbn.nativeElement.value,
+      this.title.nativeElement.value,
+      this.description.nativeElement.value
+    );
+
+    this.books.push(newBook);
+
+    // isbn.value = title.value = description.value = '';
+  }
 }

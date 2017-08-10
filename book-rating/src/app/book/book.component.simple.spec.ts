5df361070407eb4c7fde4d21f2a889f098af348c
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-08-10 11:21:05 +0200

    BookComponent spec: mit beforeEach und rateDown

--- book-rating/src/app/book/book.component.simple.spec.ts
+++ book-rating/src/app/book/book.component.simple.spec.ts
@@ -1,18 +1,28 @@
 import { BookComponent } from './book.component';
 import { Book } from '../shared/book';
 
-describe('BookComponent', () => {
-  it('should forward rateUp() to the Book entity', () => {
-    const comp = new BookComponent();
+describe('BookComponent Isolated', () => {
+
+  let comp: BookComponent;
+
+  beforeEach(() => {
+    comp = new BookComponent();
 
     comp.book = {
-      rateUp: () => { }
+      rateUp: () => { },
+      rateDown: () => { }
     } as Book;
+  });
 
+  it('should forward rateUp() to the Book entity', () => {
     spyOn(comp.book, 'rateUp');
-
     comp.rateUp();
-
     expect(comp.book.rateUp).toHaveBeenCalled();
+  });
+
+  it('should forward rateDown() to the Book entity', () => {
+    spyOn(comp.book, 'rateDown');
+    comp.rateDown();
+    expect(comp.book.rateDown).toHaveBeenCalled();
   });
 });

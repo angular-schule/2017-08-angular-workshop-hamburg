09604b0099070d2e6ceb0f1e786f58b93dad5c94
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-08-10 13:20:29 +0200

    BookComponent: Shallow Unit Test

--- book-rating/src/app/book/book.component.spec.ts
+++ book-rating/src/app/book/book.component.spec.ts
@@ -1,6 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { By } from '@angular/platform-browser';
+
 import { BookComponent } from './book.component';
+import { Book } from '../shared/book';
+
 
 describe('BookComponent', () => {
   let component: BookComponent;
@@ -16,10 +20,31 @@ describe('BookComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(BookComponent);
     component = fixture.componentInstance;
+
+    component.book = {
+      rating: 1,
+      rateUp: () => { },
+      rateDown: () => { }
+    } as Book;
+
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('clicking "thumbs up" should call rateUp()', () => {
+    const rateUpBtn = fixture.debugElement.query(By.css('button.rateUpBtn')).nativeElement;
+    spyOn(component, 'rateUp');
+    rateUpBtn.click();
+    expect(component.rateUp).toHaveBeenCalled();
+  });
+
+  it('clicking "thumbs down" should call rateDown()', () => {
+    const rateDownBtn = fixture.debugElement.query(By.css('button.rateDownBtn')).nativeElement;
+    spyOn(component, 'rateDown');
+    rateDownBtn.click();
+    expect(component.rateDown).toHaveBeenCalled();
   });
 });

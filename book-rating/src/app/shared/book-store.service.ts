dd9c1de113bec0280879941d945be16666e3456c
Ferdinand Malcher <mail@ferdinand-malcher.de>
2017-08-11 10:53:31 +0200

    Service mit Interface BookResponse

--- book-rating/src/app/shared/book-store.service.ts
+++ book-rating/src/app/shared/book-store.service.ts
@@ -4,15 +4,18 @@ import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 import { Book } from './book';
+import { BookResponse } from './book-response';
 
 @Injectable()
 export class BookStoreService {
 
+  apiUrl = 'https://api.angular.schule';
+
   constructor(private http: HttpClient) { }
 
   getAll(): Observable<Book[]> {
-    return this.http.get('https://api.angular.schule/books')
-      .map((rawBooks: any[]) => rawBooks.map(
+    return this.http.get<BookResponse[]>(`${this.apiUrl}/books`)
+      .map(rawBooks => rawBooks.map(
         rawBook => new Book(rawBook.isbn, rawBook.title, rawBook.description, rawBook.rating))
       );
   }
